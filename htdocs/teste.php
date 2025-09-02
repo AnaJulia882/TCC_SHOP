5a92e26da49227ae3c688b530c8dee8369fdee47
AnaJulia882 <univapanajulia@gmail.com>
2025-09-01 22:18:02 -0300

    Delete htdocs directory

--- htdocs/teste.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = mysqli_connect('localhost', 'root', '', 'shop_db');
-
-if (!$conn) {
-    die('Erro ao conectar: ' . mysqli_connect_error());
-} else {
-    echo 'Conexão OK!';
-}
-?>
